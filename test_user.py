d414f2382ba075b634e35b8774e13d6e20f66af5
imekenye <ismekenye@gmail.com>
2019-04-05 12:20:27 -0700

    Add testcase to display user list

--- test_user.py
+++ test_user.py
@@ -58,6 +58,12 @@ class TestUser(unittest.TestCase):
         self.new_user.delete_user()
         self.assertEqual(len(User.user_list), 1)
 
+    def test_display_users(self):
+        """
+        test_display_users to test display of user details
+        """
+        self.assertEqual(User.display_users(), User.user_list)
+
 
 if __name__ == '__main__':
     unittest.main()
