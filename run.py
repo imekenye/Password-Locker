62272b08c0bdfad0e9bbc3ea888663193e18df0f
imekenye <ismekenye@gmail.com>
2019-04-06 10:39:37 -0700

    Add create_user function to create username and password

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3.6
 from user import User
 from credential import Credential
+
+
+def create_user(name, passw):
+    """
+    Function to create a new user
+    """
+    new_user = User(name, passw)
+    return new_user
