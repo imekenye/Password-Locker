8ae2c8159748abad56120e4ab3ad2f3b174e0635
imekenye <ismekenye@gmail.com>
2019-04-06 14:07:28 -0700

    Add main function in run.py file and import random

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 from user import User
 from credential import Credential
+from random import *
 
 
 def create_user(name, passw):
@@ -48,8 +49,115 @@ def display_credentials():
     return Credential.display_credential()
 
 
-def generate_password(User):
+def generate_password(num):
     """
     Function that generates random passwords
     """
-    return User.generate_pass()
+    for i in range(num):
+        return random.randint(0, 100)
+
+
+def main():
+    print("Welcome.What is your name?")
+    myName = input()
+
+    print(f"Hello {myName}!")
+    print('\n')
+
+    while True:
+        print("Type in a number according to what you would want to do:\n 1 - Create account \n 2 - Log In \n 3 - exit")
+        chosen = int(input())
+        print('\n')
+
+        if chosen == 1:
+            print("Enter username:")
+            uName = input()
+            print("Do you want to auto generate a password? Y/N ")
+            while True:
+                answr = input().upper()
+                if answr == 'Y':
+                    passwrd = generate_password(10)
+                    print(f"This is your password - {passwrd}")
+                    break
+                elif answr == 'N':
+                    print("Enter your password for the account")
+                    userPass = input()
+                    print(f"This is your password - {userPass}")
+                    break
+                else:
+                    print("please choose Y or N")
+
+            save_users(create_user(uName, userPass))
+            print('\n')
+            print(f"Your new Username is --- {uName}")
+            print('\n')
+        elif chosen == 2:
+            print("Please enter username?")
+            uName = input()
+            print("Password please?")
+            uPass = input()
+
+            if not User.user_list:  # check if list is empty
+                print('\n')
+                print("Sorry we do not have you in our list!")
+
+            for user in User.user_list:
+
+                if uName == user.username and uPass == user.password:
+                    print("Login Success!!")
+                    print('\n')
+
+                    print(f"Welcome {uName}")
+
+                    while True:
+                        print(
+                            "Choose number according to your needs: 1 - create new credential \n 2 - Display Credentials \n 3 - delete user")
+
+                        chosenOne = int(input())
+
+                        if chosenOne == 1:
+                            print("Enter username")
+                            usName = input()
+                            print("Enter email")
+                            mailName = input()
+                            print("Auto-generate password? Y/N")
+                            while True:
+                                answer = input().upper()
+                                if answer == 'Y':
+                                    passwrd = generate_password(8)
+                                    print(
+                                        f"Your password is - {passwrd}")
+                                    break
+                                elif answer == 'N':
+                                    print("Enter password")
+                                    credentialpass = input()
+                                    print(
+                                        f"Your Password is - {credentialpass} ")
+                                    break
+                                else:
+                                    print(
+                                        "You have to choose something..here we go again Y/N")
+                            save_credentials(create_credential(
+                                usName, mailName, credentialpass))
+
+                            print('\n')
+                            print(f"Credentials saved!")
+                            print('\n')
+                        elif chosenOne == 2:
+                            if display_credentials():
+                                print('\n')
+
+                                for credential in display_credentials():
+                                    print(
+                                        f"{myName}'s credentials:- \n {credential.username} - {credential.email} - { credential.password}")
+                                    print('\n')
+                            else:
+                                print("No user credentials!")
+                                print('\n')
+                        elif chosenOne == 3:
+                            print("")
+
+
+if __name__ == '__main__':
+
+    main()
